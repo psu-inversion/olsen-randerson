e836c613088ce8e810eb480369e178cdd1299257
Daniel Wesloh <22566757+DWesl@users.noreply.github.com>
2019-08-28 13:08:09 -0400

    Add documentation to the package.
    So far very basic, nothing beyond an invocation of sphinx-apidoc some
    tidying and the addition of extlinks.

---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. src documentation master file, created by
+   sphinx-quickstart on Wed Aug 28 12:26:51 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to olsen-randerson's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   olsen_randerson
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/olsen_randerson/__init__.py
+++ src/olsen_randerson/__init__.py
@@ -4,7 +4,7 @@ import numpy as np
 NEP_TO_GPP_FACTOR = 2
 """Conversion factor to estimate GPP from NEE."""
 Q10 = 1.5
-"""The $Q_{10}$ value used in the Olsen and Randerson downscaling.
+"""The :math:`Q_{10}` value used in the Olsen and Randerson downscaling.
 
 It is the factor by which the respiration increases when the
 temperature increases by ten degrees.
@@ -21,17 +21,26 @@ def olsen_randerson_once(flux_nep, temperature, par):
 
     Parameters
     ----------
-    flux_nee: np.ndarray[...]
-        Biogenic Net Ecosystem Productivity, usually at monthly timescale
-    temperature: np.ndarray[N, ...]
-        Temperature in Celsius, at the downscaled resolution
-    par: np.ndarray[N, ...]
+    flux_nee : np.ndarray[...]
+        Biogenic Net Ecosystem Productivity, usually at monthly timescale.
+        Units must include time in the denominator.
+    temperature : np.ndarray[N, ...]
+        Temperature in Celsius, at the downscaled resolution.
+        Units are expected to be degrees Celsius.
+    par : np.ndarray[N, ...]
         Photosynthetically Active Radiation, at the downscaled resolution
 
     Returns
     -------
-    flux_nee: np.ndarray[N, ...]
+    flux_nee : np.ndarray[N, ...]
         The downscaled NEE
+
+    References
+    ----------
+    Olsen, Seth C. and James T. Randerson, 2004.  "Differences between
+    surface and column atmospheric CO2 and implications for carbon
+    cycle research," *Journal of Geophysical Research: Atmospheres*
+    vol. 109, no. D2, D02301.  :doi:`10.1029/2003JD003968`.
 
     Examples
     --------
@@ -47,13 +56,6 @@ def olsen_randerson_once(flux_nep, temperature, par):
            23.70071827, 18.33559721,  4.10768819, -4.2353102 , -3.4581163 ,
            -3.20043607, -3.4581163 , -4.2353102 ,  4.10768819, 18.33559721,
            23.70071827, 18.33559721,  4.10768819, -4.2353102 , -3.4581163 ])
-
-    References
-    ----------
-    Olsen, Seth C. and James T. Randerson, 2004.  "Differences between
-    surface and column atmospheric CO2 and implications for carbon
-    cycle research," *Journal of Geophysical Research: Atmospheres*
-    vol. 109, no. D2, D02301.  :doi:`10.1029/2003JD003968`.
     """
     estimated_gpp = NEP_TO_GPP_FACTOR * flux_nep
     flux_gpp = olsen_randerson_gpp_once(
@@ -70,19 +72,17 @@ def olsen_randerson_gpp_once(flux_gpp, par):
 
     Parameters
     ----------
-    flux_gpp: np.ndarray[...]
-    par: np.ndarray[N, ...]
+    flux_gpp : np.ndarray[...]
+        The Gross Primary Productivity to downscale.
+        Units must include time in denominator.
+    par : np.ndarray[N, ...]
+        Photosynthetically active radiation at downscaled frequency.
+        Will be used for the GPP downscaling.
 
     Returns
     -------
-    flux_gpp: np.ndarray[N, ...]
-
-    Examples
-    --------
-    >>> olsen_randerson_gpp_once(np.array(2.), np.array([0., 1, 1]))
-    array([0., 3., 3.])
-    >>> olsen_randerson_gpp_once(np.array(2.), np.array([0., 2, 2]))
-    array([0., 3., 3.])
+    flux_gpp : np.ndarray[N, ...]
+        The downscaled GPP
 
     References
     ----------
@@ -90,6 +90,13 @@ def olsen_randerson_gpp_once(flux_gpp, par):
     surface and column atmospheric CO2 and implications for carbon
     cycle research," *Journal of Geophysical Research: Atmospheres*
     vol. 109, no. D2, D02301.  :doi:`10.1029/2003JD003968`.
+
+    Examples
+    --------
+    >>> olsen_randerson_gpp_once(np.array(2.), np.array([0., 1, 1]))
+    array([0., 3., 3.])
+    >>> olsen_randerson_gpp_once(np.array(2.), np.array([0., 2, 2]))
+    array([0., 3., 3.])
     """
     par_total = par.mean(axis=0)
     return (flux_gpp / par_total)[np.newaxis, ...] * par
@@ -100,17 +107,17 @@ def olsen_randerson_resp_once(flux_resp, temperature):
 
     Parameters
     ----------
-    flux_resp: np.ndarray[...]
-    temperature: np.ndarray[N, ...]
+    flux_resp : np.ndarray[...]
+        The original respiration fluxes to downscale.
+        Units must include time in the denominator.
+    temperature : np.ndarray[N, ...]
+        The temperature at the downscaled frequency.
+        Expected units are degrees Celsius.
 
     Returns
     -------
-    flux_resp: np.ndarray[N, ...]
-
-    Examples
-    --------
-    >>> olsen_randerson_resp_once(np.array(19./12.), np.array([0., 10., 20.]))
-    array([1.  , 1.5 , 2.25])
+    flux_resp : np.ndarray[N, ...]
+        The downscaled respiration fluxes.
 
     References
     ----------
@@ -118,6 +125,11 @@ def olsen_randerson_resp_once(flux_resp, temperature):
     surface and column atmospheric CO2 and implications for carbon
     cycle research," *Journal of Geophysical Research: Atmospheres*
     vol. 109, no. D2, D02301.  :doi:`10.1029/2003JD003968`.
+
+    Examples
+    --------
+    >>> olsen_randerson_resp_once(np.array(19./12.), np.array([0., 10., 20.]))
+    array([1.  , 1.5 , 2.25])
     """
     resp_scaling = Q10 ** ((temperature - T0) / 10)
     resp_total = resp_scaling.mean(axis=0)
