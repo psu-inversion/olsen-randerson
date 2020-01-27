452811be36be8af1403eaa1f3231941887dc13a2
DWesl <22566757+DWesl@users.noreply.github.com>
2020-01-27 11:13:31 -0500

    Try specifying the local directory when writing __version__.py

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@
 Boilerplate code needed to distribute package and include version in
 package.
 """
+import os.path
+
 from setuptools import setup
 
-with open("VERSION", "r") as in_file:
-    with open("src/olsen_randerson/__version__.py", "w") as out_file:
+try:
+    THIS_DIR = os.path.dirname(__file__)
+except NameError:
+    THIS_DIR = os.getcwd()
+
+with open(os.path.join(THIS_DIR, "VERSION"), "r") as in_file:
+    with open(
+        os.path.join(
+            THIS_DIR, "src", "olsen_randerson", "__version__.py"
+        ), "w"
+    ) as out_file:
         out_file.write("""\"\"\"Version for the package.\"\"\"
 VERSION = "{ver:s}"
 """.format(ver=in_file.read()))
